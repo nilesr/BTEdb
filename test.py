05226a8d3c1b3e92a2a08fde2ff07229c109c382
nilesr <nilesrogoff@gmail.com>
2014-10-18 16:40:23 -0400

    Updated example

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-import sys, json
-sys.path += "."
-import database
-db = database.Database("x.json", 4)
+import sys
+import JsonDB
+db = JsonDB.Database("x.json", 4)
 def putData():
     db.Create("main table")
     db.Create("birthdays")
@@ -11,7 +10,4 @@ def putData():
     db.Insert("birthdays", username = "root", bday = 1992)
     db.Insert("birthdays", username = "Ethan", bday = 1999)
     db.Insert("birthdays", username = "Niles", bday = 1998)
-def printData():
-    print json.dumps(db.Dump(), indent = 2)
-db.Dump()
-
+# Do stuff here
